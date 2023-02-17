645f5be5213e1622a78e2e18fcf69c2bca18f614
gilrjunior <gilmarjunioreis@gmail.com>
2023-02-17 19:15:17 -0300

    Aula 12 - Parte 2

---
+++ Aula12/ex012.js
@@ -0,0 +1,16 @@
+time = new Date().getHours()
+
+if(time < 12){
+
+    console.log("Bom dia!")
+
+}else if(time >= 12 && time < 18){
+
+    console.log("Boa tarde!")
+
+}else{
+
+    console.log("Boa Noite!")
+
+
+}
