bd781c790e866348c79b402603f52b6bd276014a
gilrjunior <gilmarjunioreis@gmail.com>
2023-02-17 18:45:02 -0300

    Aula 11 - Parte 1

---
+++ Aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 90
+
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 80) {
+
+    console.log(`Você está a ${vel} km/h. Essa velocidade é acima do permitido, então você foi multado!`)
+
+}
+
+console.log(`Use sempre o cinto de segurança`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,13 @@
+var pais = 'Brasil'
+
+console.log(`Seu pais de origem é ${pais}`)
+
+if (pais == 'Brasil' || pais == 'Brazil') {
+
+    console.log("Você é brasileiro!")
+
+}else{
+
+    console.log("Você é estrangeiro!")
+
+}
