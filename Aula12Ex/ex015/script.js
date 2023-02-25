2e63daa7ef5331b425b2ba2cbaa2b6ff3d70a17b
gilrjunior <gilmarjunioreis@gmail.com>
2023-02-24 22:04:32 -0300

    Ex0015

--- Aula12Ex/ex015/script.js
+++ Aula12Ex/ex015/script.js
@@ -1,1 +1,39 @@
-alert("Hello!")
+function verificar() {
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById("txtano")
+    var res = document.getElementById("res")
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+
+        alert("ERRO! INFORME CORRETAMENTE OS DADOS")
+
+    } else {
+
+        var idade = ano - Number(fano.value)
+        var fase = " "
+        fsex = document.getElementsByName("radsex")
+
+        var genero = fsex[0].checked ? "m" : "f"
+        var gen = genero == "m" ? "Homem" : "Mulher"
+        var img = document.createElement("img")
+        img.setAttribute("id", "pic")
+
+        if (idade >= 0 && idade <= 10) {
+            fase = "crianca"
+        } else if (idade < 21) {
+            fase = "jovem"
+        } else if (idade < 50) {
+            fase = "adulto"
+        } else {
+            fase = "idoso"
+        }
+
+        img.setAttribute("src", `./img/foto-${fase}-${genero}.png`)
+
+        res.innerHTML = `<p>Você é ${gen} e em ${ano} você completa ${idade} anos de idade!</p>`
+        res.appendChild(img)
+
+    }
+}
